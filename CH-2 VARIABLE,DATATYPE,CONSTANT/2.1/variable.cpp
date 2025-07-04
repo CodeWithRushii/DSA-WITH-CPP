bf72fa0872acb54b4b7bfc7d116566e2a9bfdb53
unknown <dedaniyaniyarushi4@gmail.com>
2025-07-04 10:49:12 +0530

    CH-5 CONTROL STATMENT

--- CH-2 VARIABLE,DATATYPE,CONSTANT/2.1/variable.cpp
+++ CH-2 VARIABLE,DATATYPE,CONSTANT/2.1/variable.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main()
+main()
 {
     cout<<"daatype, constant , variable"<<endl
     <<endl;
--- CH-4 OPERATOR/QUESTION/Q4.cpp
+++ CH-4 OPERATOR/QUESTION/Q4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main()
+main()
 {
     int x, y;
     cout << "Enter value of x: ";
