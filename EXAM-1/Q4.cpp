33fff21ec088158fb77b8a65446b9cdae99a9868
unknown <dedaniyaniyarushi4@gmail.com>
2025-07-09 11:38:55 +0530

    EXM-1

---
+++ EXAM-1/Q4.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int score;
+    char grade;
+
+    cout << "Enter your score: ";
+    cin >> score;
+
+    grade = (score >= 90) 
+        ? 'A'
+        : (score >= 80) 
+        ? 'B'
+        : (score >= 70)   
+        ? 'C'
+        : (score >= 60)  
+        ? 'D'
+        : (score >= 50) 
+        ? 'E'
+        : 'F';
+        
+    cout << "grade is: " << grade << endl;
+
+    switch (grade)
+    {
+    case 'A':
+        cout << "Excellent work!" << endl;
+        break;
+    case 'B':
+        cout << "Good job!" << endl;
+        break;
+    case 'C':
+        cout << "You passed." << endl;
+        break;
+    case 'D':
+        cout << "You need to improve." << endl;
+        break;
+    case 'F':
+        cout << "Failed. Better luck next time." << endl;
+        break;
+    default:
+        cout << "Invalid grade." << endl;
+    }
+
+    if (score >= 60)
+    {
+        cout << "You are eligible for the next level." << endl;
+    }
+    else
+    {
+        cout << "You need to re-try in the exam." << endl;
+    }
+}
