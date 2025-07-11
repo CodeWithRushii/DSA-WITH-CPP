20e6d19df923f7beffd27d0edcdb9a46874c8736
unknown <dedaniyaniyarushi4@gmail.com>
2025-07-11 10:45:37 +0530

    CH-6 LOOPING

---
+++ CH-6 LOOPING/LOOP_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int i = 1;
+    int j;
+
+    cout << "enter your no :-";
+    cin >> j;
+
+    while (i<= j)
+        cout << i++ <<"\t";
+
+}
