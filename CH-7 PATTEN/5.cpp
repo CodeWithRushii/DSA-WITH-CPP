4d4c1dc4255734f8cf2b9fe2dbaa58a6aa0acf59
unknown <dedaniyaniyarushi4@gmail.com>
2025-07-21 10:52:13 +0530

    CH-7 PATTEN

---
+++ CH-7 PATTEN/5.cpp
@@ -0,0 +1,16 @@
+// 1 11 111 11111 11111
+#include <iostream>
+using namespace std;
+
+main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "1";
+        }
+        cout << ",";
+    }
+    return 0;
+}
